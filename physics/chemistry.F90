20490769f453208e5a8e76b5d38447530d236403
Cheryl Craig <cacraig@ucar.edu>
2018-06-15 11:03:22 -0600

    simplified chemistry_run list calling list

--- physics/chemistry.F90
+++ physics/chemistry.F90
@@ -90,7 +90,7 @@ contains
 !! | local_name | standard_name                                    | long_name                               | units   | rank | type      | kind      | intent | optional |
 !! |------------|--------------------------------------------------|-----------------------------------------|---------|------|-----------|-----------|--------|----------|
 !! | co         | my_volume_mixing_ratio_co                        | CO volume mixing ratio                  | kg kg-1 |    1 | real      | kind_phys | inout  | F        |
-!! | errmsg     | error_message                                    | CCPP error message                      | none    |    0 | character | len=*     | out    | F        |
+!! | errmsg     | error_message                                    | CCPP error message                      | none    |    0 | character | len=512   | out    | F        |
 !! | errflg     | error_flag                                       | CCPP error flag                         | flag    |    0 | integer   |           | out    | F        |
 !!
   subroutine chemistry_init (co, errmsg, errflg)
@@ -99,7 +99,7 @@ contains
 
     !--- arguments
     real(rk),pointer, intent(inout) :: co(:)
-    character(len=*), intent(out)   :: errmsg
+    character(len=512), intent(out)   :: errmsg
     integer,          intent(out)   :: errflg
 
     co(:) = 100_rk
@@ -112,22 +112,23 @@ contains
 !> \section arg_table_chemistry_run Argument Table
 !! | local_name | standard_name                                    | long_name                               | units   | rank | type      | kind      | intent | optional |
 !! |------------|--------------------------------------------------|-----------------------------------------|---------|------|-----------|-----------|--------|----------|
-!! | dt         | time_step_for_physics                            | physics time step                       | s       |    0 | real      | kind_phys | in     | F        |
-!! | ncol       | horizontal_loop_extent                           | horizontal dimension                    | count   |    0 | integer   |           | in     | F        |
-!! | nlev       | adjusted_vertical_layer_dimension_for_radiation  | number of vertical layers for radiation | count   |    0 | integer   |           | in     | F        |
 !! | co         | my_volume_mixing_ratio_co                        | CO  volume mixing ratio                 | kg kg-1 |    1 | real      | kind_phys | inout  | F        |
-!! | errmsg     | error_message                                    | CCPP error message                      | none    |    0 | character | len=*     | out    | F        |
+!! | errmsg     | error_message                                    | CCPP error message                      | none    |    0 | character | len=512   | out    | F        |
 !! | errflg     | error_flag                                       | CCPP error flag                         | flag    |    0 | integer   |           | out    | F        |
 !!
-  subroutine chemistry_run (dt, ncol,nlev, co, errmsg, errflg)
+!*! | dt         | time_step_for_physics                            | physics time step                       | s       |    0 | real      | kind_phys | in     | F        |
+!*! | ncol       | horizontal_loop_extent                           | horizontal dimension                    | count   |    0 | integer   |           | in     | F        |
+!*! | nlev       | adjusted_vertical_layer_dimension_for_radiation  | number of vertical layers for radiation | count   |    0 | integer   |           | in     | F        |
+!  subroutine chemistry_run (dt, ncol,nlev, co, errmsg, errflg)
+  subroutine chemistry_run ( co, errmsg, errflg)
 
     implicit none
 
     !--- arguments
-    real(rk),         intent(in)    :: dt
-    integer,          intent(in)    :: ncol,nlev
+!    real(rk),         intent(in)    :: dt
+!    integer,          intent(in)    :: ncol,nlev
     real(rk), pointer,intent(inout) :: co(:)      
-    character(len=*), intent(out)   :: errmsg
+    character(len=512), intent(out)   :: errmsg
     integer,          intent(out)   :: errflg
 
     !--- local variables
